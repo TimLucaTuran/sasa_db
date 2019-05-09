424d04cd062e486d45a1ca4e803f1557427fe682
Tim <timturan@web.de>
2019-05-09 12:55:49 +0200

    added adress system, queries not accepted jet need to turn sql_list into a dict for a cleaner structure

--- sql.py
+++ sql.py
@@ -25,10 +25,11 @@ sql_list=[['geometry', None],
           ['wavelength_stop', None],
           ['spectral_points', None],
           ['simulation_order', None],
+          ['adress', None],
           ['length', None],
           ['width', None],
           ['thickness', None],
-          ['corner_radius', 'Null'],
+          ['corner_radius', None],
           ['radius', None],
           ['rounded_corner', None],
           ['hole', None],
@@ -101,6 +102,11 @@ class Exl:
 
         return
 
+    def geo_features(self):
+        if 'rounded' in self.data:
+            self.target_list[-2][1] = 1
+        return
+
 class QueryGenerator:
     def __init__(self):
         self.sim_query = 'INSERT INTO simulations ('
@@ -111,16 +117,21 @@ class QueryGenerator:
         self.geometries = {'wire' : self.wire,
                            'square' : self.square,
                            }
+        self.adress = []
 
     def make_query(self, sql_list):
+        sim_count = 0
+        geo_count = 0
         #simulations query
-        for col in sql_list[1:10]:
-            self.sim_query += col[0]
-            self.sim_query += ', '
+        for col in sql_list[1:11]:
+            if col[1] is not None:
+                sim_count += 1
+                self.sim_query += col[0]
+                self.sim_query += ', '
 
         self.sim_query = self.sim_query[:-2]
         self.sim_query += ') VALUES ('
-        self.sim_query += 9* '%s, '
+        self.sim_query += sim_count* '%s, '
         self.sim_query = self.sim_query[:-2]
         self.sim_query += ')'
 
@@ -138,22 +149,76 @@ class QueryGenerator:
         active_cols = [col for col in sql_list[10:] if col[0] in current_geo]
 
         for col in active_cols:
-            self.geo_query += col[0]
-            self.geo_query += ', '
+            if col[1] is not None:
+                geo_count += 1
+                self.geo_query += col[0]
+                self.geo_query += ', '
 
         self.geo_query = self.geo_query[:-2]
         self.geo_query += ') VALUES ('
-        self.geo_query += len(active_cols)* '%s, '
+        self.geo_query += geo_count* '%s, '
         self.geo_query = self.geo_query[:-2]
         self.geo_query += ')'
+        print(self.sim_query)
+        for d in sql_list[1:11]:
+            print(d)
+        print(self.geo_query)
+        for d in sql_list[11:]:
+            print(d)
+        print('\n')
+        self.sim_query = 'INSERT INTO simulations ('
+        self.geo_query = 'INSERT INTO '
+
+        #Bad structure here: Turn the adress list into a str
+        sql_list[10][1] = str(sql_list[10][1])
+        #Try to execute the queries
+        try:
+            my_cursor.execute(self.sim_query,
+                     tuple([col[1] for col in sql_list if col[1] is not None]))
+        except Exception as e:
+            print('Bad simulation query')
+            print(e)
+
         return
+
+    def loop_sql_lists(self, sql_list, list_cols):
+        if len(list_cols) == 0:
+            print('adress: ', self.adress)
+            self.make_query(sql_list)
+            return
+        else:
+            print('loop called')
+            tup = list_cols.pop()
+            current_list = tup[0]
+            current_index = tup[1]
+            self.adress.append(None)
+
+            for i in range(len(current_list)):
+                sql_list[current_index][1] = current_list[i]
+                self.adress[-1] = i
+                sql_list[10][1] = self.adress
+                self.loop_sql_lists(sql_list, list_cols)
+
+            #reset the adress
+            sql_list[10][1] = None
+            return
+
+
 
     def generate(self, sql_list):
         """
         This method gets a possibly nested sql_list.
         It splits them up and calls make_query for every simple list.
         """
-        
+        #find the colums which have list-values
+        list_cols = [(col[1], sql_list.index(col)) for col in sql_list if type(col[1]) is list]
+        list_cols.sort(key = lambda tup : len(tup[0]), reverse = True)
+        print('list_cols: ', list_cols)
+        #reset the adress
+        self.adress = []
+        self.loop_sql_lists(sql_list, list_cols)
+        return
+
 
 
 #Define the excel_list with one Exl for every column
@@ -161,14 +226,15 @@ exl_list = [Exl('m-file', 1, ),
             Exl('material', 2),
             Exl('cladding', 3),
             Exl('substrate', 4),
-            Exl('geom', 0),
+            Exl('geom', 0, [Exl.geo_features]),
             Exl('periode', 5),
             Exl('wavelength', 6, [Exl.wav_split]),
             Exl('points', 8),
             Exl('order', 9),
-            Exl('length', 10, [Exl.evaluate, Exl.listify]),
-            Exl('width', 11, [Exl.evaluate, Exl.listify]),
-            Exl('thickness', 12, [Exl.evaluate, Exl.listify]),
+            Exl('length', 11, [Exl.evaluate, Exl.listify]),
+            Exl('width', 12, [Exl.evaluate, Exl.listify]),
+            Exl('thickness', 13, [Exl.evaluate, Exl.listify]),
+            Exl('corner radius', 14),
             ]
 
 
@@ -187,8 +253,9 @@ for cell in name_row:
 
 ####Main loop: Generate SQL-queries for every Excel row
 query_gen = QueryGenerator()
-for row in ws.iter_rows(name_row[0].row + 1, 4):
+for row in ws.iter_rows(name_row[0].row + 1, 8):
     #Load the data of the row
+    print('row: ', row[0].row)
     for i in range(len(row)):
         for exl in exl_list:
             if i == exl.column:
@@ -199,9 +266,8 @@ for row in ws.iter_rows(name_row[0].row + 1, 4):
             opperation(exl)
         exl.write()
 
-    query_gen.make_query(sql_list)
-    print(query_gen.sim_query)
-    print(query_gen.geo_query)
+    query_gen.generate(sql_list)
+
 
 
 
