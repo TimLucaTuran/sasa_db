7ed8d07641fd51e0377a843153b17a54685d40fa
Tim <timturan@web.de>
2019-07-16 12:38:03 +0200

    added extract_params()

--- crawler.py
+++ crawler.py
@@ -2,9 +2,15 @@ import subprocess
 from scipy.io import loadmat
 import mysql.connector as mc
 
+def mat_print(mat):
+    for i in range(4):
+        print('{:+.2f} {:+.2f} {:+.2f} {:+.2f}'.format(mat[i, 0], mat[i,1], mat[i,2], mat[i,3]))
+
 class Crawler:
     def __init__(self, directory):
         self.directory = directory
+        self.target_dict = None
+        self.cursor = None
 
     def find_path(self, name):
         bashCommand = 'find {} -name *{}*.mat -print'.format(self.directory, name)
@@ -14,13 +20,18 @@ class Crawler:
         path = output[:-1].decode('UTF-8')
         return path
 
-    def find(self, name):
-        path = self .find_path(name)
-        smat = loadmat(path)
-        print(smat['SMAT_'].shape)
-        return smat
 
-    def extract_all(self, target_dict):
+    def find(self, name, adress=None):
+        path = self.find_path(name)
+        smat = loadmat(path)['SMAT_']
+        if adress is None:
+            return smat
+        else:
+            wav_length_dim = smat.shape[-3]
+            adress += [slice(wav_length_dim), slice(4), slice(4)]
+            return smat[tuple(adress)]
+
+    def extract_all(self):
         mydb = mc.connect(
             host = "localhost",
             user = "root",
@@ -39,8 +50,26 @@ class Crawler:
             output, error = process.communicate()
             print(output, error)
 
-crawler = Crawler(directory='/run/media/tim/D4C5-A3BA/')
-#%%
 
-crawler.find('Chi_RotWire_1_rounded_Ti_n')
-crawler.extract_all(target_dict='/home/tim/Desktop/smat_test')
+    def extract_params(self,name):
+        mydb = mc.connect(
+            host = "localhost",
+            user = "root",
+            passwd = "tltit69z",
+            database = "meta_materials",
+        )
+        cursor = mydb.cursor()
+        query = '''SELECT  particle_material, cladding, substrate, periode, wavelength_start,
+        wavelength_stop FROM simulations WHERE m_file = "{}"'''.format(name)
+        cursor.execute(query)
+        row = cursor.fetchone()
+        return row
+
+#%%
+if __name__ == '__main__':
+
+    crawler = Crawler(directory='/run/media/tim/D4C5-A3BA/')
+    mat = crawler.find('Chi_RotWire_1_rounded_Ti_n', adress=[1,1,1])
+    mat_print(mat[0,:,:])
+    crawler.extract_params('Chi_RotWire_1_rounded_Ti_n')
+    #crawler.extract_all(target_dict='/home/tim/Desktop/smat_test')
