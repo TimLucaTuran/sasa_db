b6b918f89a3b994195f5d24f20b254cd71e8278a
tim <timturan@web.de>
2019-10-11 13:00:11 +0200

    added a db-rebuild script

---
+++ db_rebuild.py
@@ -0,0 +1,9 @@
+import os
+
+os.system("rm meta_materials.db")
+os.system("python db_setup.py")
+
+exl_sheets = [10, 11, 13, 14, 16, 22]
+for sheet in exl_sheets:
+    print("working on sheet ", sheet)
+    os.system("python exl_to_sql.py {}".format(sheet))
